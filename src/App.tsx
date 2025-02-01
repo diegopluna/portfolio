bdf87bec18dbaed287c169e1ea187cd8e7107656
Diego Peter Luna <dpl@cesar.school>
2025-02-01 14:04:36 -0300

    fix: update App component to temporarily remove Terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import {Terminal} from './components/terminal'
+import { Terminal } from "./components/terminal";
 
 function App() {
-
   return (
     <div className="min-h-screen p-4 sm:p-6 md:p-8 flex flex-col items-center justify-center bg-gradient-to-br from-gray-900 to-gray-800">
       <div className="w-full h-[calc(100vh-4rem)] max-w-4xl mx-auto">
-      <Terminal />
+        Nothing to see here
+        {/* <Terminal /> */}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
